f41e669b3e90fab80a0849103b36d25eab0e611d
Athanasia Farmaki <farmaki@kth.se>
2025-05-27 17:00:40 +0200

    restart project

--- src/encoders_decoders.rs
+++
@@ -1,9 +0,0 @@
-
-use spin_sdk::http::Request;
-
-use crate::encoders_decoders_objects::AddToQueueRequest;
-
-
-pub fn decode_request(req:Request)->AddToQueueRequest {
-    AddToQueueRequest::new(req)
-}
