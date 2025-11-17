34f54c4816f38a77e716839d60de033adace6bfb
Athanasia Farmaki <farmaki@kth.se>
2025-11-17 16:54:33 +0100

    delete unused file

--- build.sh
+++
@@ -1,4 +0,0 @@
-cargo build --release --target aarch64-unknown-linux-gnu
-aarch64-unknown-linux-gnu-strip target/aarch64-unknown-linux-gnu/release/rise-thesis
-
-du -h target/aarch64-unknown-linux-gnu/release/rise-thesis
